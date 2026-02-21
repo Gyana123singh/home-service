ca0210993c21d3e92d497ea48ca59879ff2e422d
Gyana Singh <gyan123priya@gmail.com>
2026-02-21 15:45:50 +0530

    hii

--- routes/vendorRoutes/index.js
+++ routes/vendorRoutes/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 // Import all admin routes
-const vendorProfileRoutes = require("./vendorRouter");
+const vendorRoutes = require("./vendorRouter");
 const vendorKycRoutes = require("./vendorKycRoutes");
 // const vendorServiceRoutes = require("./vendorServiceRoutes");
 // const vendorBookingRoutes = require("./vendorBookingRoutes");
 
 // Mount routes
-router.use("/auth", vendorProfileRoutes);
+router.use("/", vendorRoutes);
 router.use("/categories", vendorKycRoutes);
 // router.use("/service", vendorServiceRoutes);
 // router.use("/booking", vendorBookingRoutes);
